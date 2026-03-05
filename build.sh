7ba9996cd497ad4cfb79abd6aaf177b879854bba
kibetnathan <kibetnat@gmail.com>
2026-03-06 00:17:43 +0300

    update build script to create superuser

--- build.sh
+++ build.sh
@@ -2,7 +2,23 @@
 set -o errexit
 
 pip install -r requirements.txt
-
 python manage.py collectstatic --no-input
-
 python manage.py migrate
+
+python manage.py shell <<END
+from django.contrib.auth.models import User
+import os
+
+username = 'backend_admin'
+email = os.getenv('BACKEND_ADMIN_EMAIL')
+password = os.getenv('BACKEND_ADMIN_PASSWORD')
+
+if email and password:
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username, email, password)
+        print('✓ Superuser created')
+    else:
+        print('✓ Superuser already exists')
+else:
+    print('⚠ BACKEND_ADMIN_EMAIL or BACKEND_ADMIN_PASSWORD not set')
+END
